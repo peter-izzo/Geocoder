d1c7320d2f2460a41fc60044fe326b2fc62f0f6b
Peter Izzo <peter.izzo528@gmail.com>
2019-02-27 06:26:27 -0500

    added a function to read uploaded csv file. func to add lat/lon columns

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, send_file, send_from_directory, url_for, redirect
 from werkzeug import secure_filename
 import folium
+import csv
+import pandas
 
 app=Flask(__name__)
 
@@ -11,6 +13,28 @@ ALLOWED_EXTENSIONS = {"csv", "xls", "xlsx"}
 def allowed_file(filename):
     return '.' in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
+#read uploaded csv and add new Latitude & Longitude columns
+fields = ["Latitude", "Longitude"]
+with open(file, 'a', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(fields)
+
+map = folium.Map(location=[] , zoom_start=6)
+
+#way to add markers and location to folium...fill this in with real data later
+fg = folium.FeatureGroup(name="My Map")
+for coordinates in addresses
+fg.add_child(folium.marker(location=[], popup="This is one of your addresses", icon=folium.Icon(color='green')))
+map.add_child(fg)
+
+#use pandas to open csv, then save addresses as a variable. Use this variable to add content to lat/long columns
+data = pandas.read_csv(file)
+def readcsv(path, column_names):
+    with open(path, newline="") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            record = {address}
+
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -20,6 +44,7 @@ def success():
     global file
     if request.method=="POST":
         file=request.files["file"]
+        file.read()
         file.save(secure_filename(file.filename))
         if 'file' not in request.files:
             return render_template("index.html",
